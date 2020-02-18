f0f2b1b3ce7ac2decd436c4beb1c9a4d45031f00
Joel Amarkwei <joel.amarkwei@gmail.com>
2020-02-18 19:51:29 +0000

    add this shit and I'll change them

---
+++ gitPlayers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  gitPlayers
+//
+//  Created by The Remedy on 18/02/2020.
+//  Copyright © 2020 The Remedy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func the()  {
+        print("there is a name hidden")
+    }
+
+}
+
